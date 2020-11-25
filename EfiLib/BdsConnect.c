52d7f5979ef9fdcf5e046ddd2122ac7efac9e850
dakanji <dakanji@users.noreply.github.com>
2020-11-25 03:35:28 +0300

    Remove GFX Query and GPULib

--- EfiLib/BdsConnect.c
+++ EfiLib/BdsConnect.c
@@ -17,14 +17,6 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 #include "../refind/mystrings.h"
 #include "../refind/launch_efi.h"
 #include "../include/refit_call_wrapper.h"
-#include "../Library/GpuLib/OneLinerMacros.h"
-#include "../Library/GpuLib/nvidia.h"
-#include "../Library/GpuLib/intel.h"
-#include "../Library/GpuLib/ati.h"
-
-
-#define PCI_CLASS_DISPLAY_GFX 0x80
-#define IS_PCI_GFX(_p)  IS_CLASS3 (_p, PCI_CLASS_DISPLAY, PCI_CLASS_DISPLAY_GFX, 0)
 
 BOOLEAN FoundGOP = FALSE;
 BOOLEAN ReLoaded = FALSE;
@@ -32,69 +24,6 @@ BOOLEAN ReLoaded = FALSE;
 extern EFI_STATUS AmendSysTable (VOID);
 extern EFI_STATUS AcquireGOP (VOID);
 
-typedef enum {
-  Unknown,
-  AMD,    /* 0x1002 */
-  Intel,  /* 0x8086 */
-  Nvidia, /* 0x10de */
-  RDC,    /* 0x17f3 */
-  VIA,    /* 0x1106 */
-  SiS,    /* 0x1039 */
-  ULI     /* 0x10b9 */
-} HRDW_MANUFACTURER;
-
-CONST
-CHAR16 *get_nvidia_model (
-    UINT32 device_id,
-    UINT32 subsys_id
-) {
-  UINTN i, j;
-  CHAR16 *generic_name;
-
-    // First check exceptions table
-    if (subsys_id) {
-      for (i = 0; i < (sizeof (nvidia_card_exceptions) / sizeof (nvidia_card_exceptions[0])); i++) {
-        if ((nvidia_card_exceptions[i].device == device_id) &&
-            (nvidia_card_exceptions[i].subdev == subsys_id)) {
-          return (CHAR16 *) nvidia_card_exceptions[i].name_model;
-        }
-      }
-    }
-
-  // Then, try generic names
-  for (i = 1; i < (sizeof (nvidia_card_generic) / sizeof (nvidia_card_generic[0])); i++) {
-    if (nvidia_card_generic[i].device == device_id) {
-      if (subsys_id) {
-        for (j = 0; j < (sizeof (nvidia_card_vendors) / sizeof (nvidia_card_vendors[0])); j++) {
-          if (nvidia_card_vendors[j].device == (subsys_id & 0xffff0000)) {
-              generic_name = PoolPrint (
-                  L"%s %s",
-                  nvidia_card_vendors[j].name_model,
-                  nvidia_card_generic[i].name_model
-              );
-
-            return generic_name;
-          }
-        }
-      }
-      return (CHAR16 *) nvidia_card_generic[i].name_model;
-    }
-  }
-  return (CHAR16 *) nvidia_card_generic[0].name_model;
-}
-
-CONST CHAR16 *get_gma_model (
-    IN UINT16 DeviceID
-) {
-  UINTN i;
-
-  for (i = 0; i < (sizeof (KnownGPUS) / sizeof (KnownGPUS[0])); i++)
-  {
-    if (KnownGPUS[i].device == DeviceID)
-      return (CHAR16 *) KnownGPUS[i].name;
-  }
-  return (CHAR16 *) KnownGPUS[0].name;
-}
 
 STATIC
 EFI_STATUS
@@ -279,7 +208,6 @@ EFI_STATUS BdsLibConnectMostlyAllEfi()
     UINTN                AllHandleCountTrigger;
     UINTN                i;
     UINTN                k;
-    UINTN                x;
     UINTN                HandleCount;
     UINT32               *HandleType = NULL;
     BOOLEAN              Parent;
@@ -288,30 +216,19 @@ EFI_STATUS BdsLibConnectMostlyAllEfi()
     PCI_TYPE00           Pci;
     EFI_PCI_IO_PROTOCOL* PciIo;
 
-    HRDW_MANUFACTURER    GfxVendor;
-    UINT8                *GfxMmio;
-    UINT16               UFamily;
-    UINT16               GfxFamily;
-    UINT32               Bar0;
     CHAR16               *DeviceData = NULL;
-    CHAR16               *GfxModel   = NULL;
-    CHAR16               *CardFamily = NULL;
-    radeon_card_info_t   *info;
-
 
     UINTN      GOPCount;
     EFI_HANDLE *GOPArray         = NULL;
     CHAR16     *GopDevicePathStr = NULL;
     CHAR16     *DevicePathStr    = NULL;
 
+    UINTN  SegmentPCI;
+    UINTN  BusPCI;
+    UINTN  DevicePCI;
+    UINTN  FunctionPCI;
+    UINTN  HexIndex = 0;
 
-    UINTN               SegmentPCI;
-    UINTN               BusPCI;
-    UINTN               DevicePCI;
-    UINTN               FunctionPCI;
-    BOOLEAN             GPUDevice;
-
-    UINTN               HexIndex = 0;
 
     #if REFIT_DEBUG > 0
     if (ReLoaded) {
@@ -438,121 +355,11 @@ EFI_STATUS BdsLibConnectMostlyAllEfi()
                                         DevicePCI,
                                         FunctionPCI
                                     );
-
-                                    GPUDevice = IS_PCI_GFX(&Pci);
-                                    if (GPUDevice) {
-                                        switch (Pci.Hdr.VendorId) {
-                                            case 0x1002:
-                                                info       = NULL;
-                                                GfxVendor  = AMD;
-
-                                                x = 0;
-                                                do {
-                                                    info = &radeon_cards[x];
-                                                    if (info->device_id == Pci.Hdr.DeviceId) {
-                                                        break;
-                                                    }
-                                                } while (radeon_cards[x++].device_id != 0);
-
-                                                GfxModel    = PoolPrint (L"%s", info->model_name);
-                                                DeviceData  = PoolPrint (
-                                                    L" - GPU Device      : %s by AMD",
-                                                    GfxModel
-                                                );
-
-                                                break;
-
-                                            case 0x8086:
-                                                GfxVendor  = Intel;
-                                                GfxModel   = PoolPrint (
-                                                    L"%s",
-                                                    get_gma_model (Pci.Hdr.DeviceId)
-                                                );
-                                                DeviceData = PoolPrint (
-                                                    L" - GPU Device      : %s by Intel",
-                                                    GfxModel
-                                                );
-
-                                                break;
-
-                                            case 0x10de:
-                                                GfxVendor  = Nvidia;
-                                                Bar0       = Pci.Device.Bar[0];
-                                                GfxMmio    = (UINT8*) (UINTN) (Bar0 & ~0x0f);
-
-                                                // get card type
-                                                GfxFamily  = (REG32 (GfxMmio, 0) >> 20) & 0x1ff;
-                                                UFamily    = GfxFamily & 0x1F0;
-                                                if ((UFamily == NV_ARCH_KEPLER1) ||
-                                                    (UFamily == NV_ARCH_KEPLER2) ||
-                                                    (UFamily == NV_ARCH_KEPLER3)
-                                                ) {
-                                                    CardFamily = L"Kepler";
-                                                }
-                                                else if ((UFamily == NV_ARCH_FERMI1) ||
-                                                    (UFamily == NV_ARCH_FERMI2)
-                                                ) {
-                                                    CardFamily = L"Fermi";
-                                                }
-                                                else if ((UFamily == NV_ARCH_MAXWELL1) ||
-                                                    (UFamily == NV_ARCH_MAXWELL2)
-                                                ) {
-                                                    CardFamily = L"Maxwell";
-                                                }
-                                                else if (UFamily == NV_ARCH_PASCAL) {
-                                                    CardFamily = L"Pascal";
-                                                }
-                                                else if (UFamily == NV_ARCH_VOLTA) {
-                                                    CardFamily = L"Volta";
-                                                }
-                                                else if (UFamily == NV_ARCH_TURING) {
-                                                    CardFamily = L"Turing";
-                                                }
-                                                else if ((UFamily >= NV_ARCH_TESLA) &&
-                                                    (UFamily < 0xB0)
-                                                ) { //not sure if 0xB0 is Tesla or Fermi
-                                                    CardFamily = L"Tesla";
-                                                }
-                                                else {
-                                                    CardFamily = L"Unknown";
-                                                }
-
-                                                GfxModel = PoolPrint (
-                                                    L"%s",
-                                                    get_nvidia_model (
-                                                        ((Pci.Hdr.VendorId << 16) | Pci.Hdr.DeviceId),
-                                                        ((Pci.Device.SubsystemVendorID << 16) | Pci.Device.SubsystemID)
-                                                    )
-                                                );
-                                                DeviceData = PoolPrint (
-                                                    L" - GPU Device      : %s (%s Card Family) by NVidia",
-                                                    GfxModel,
-                                                    CardFamily
-                                                );
-
-                                                break;
-
-                                            default:
-                                            GfxVendor = Unknown;
-                                            GfxModel  = PoolPrint (
-                                                L"pci%hx[%hx]",
-                                                Pci.Hdr.VendorId,
-                                                Pci.Hdr.DeviceId
-                                            );
-                                            DeviceData = PoolPrint (
-                                                L" - GPU Device      : %s by Unknown Vendor",
-                                                GfxModel
-                                            );
-
-                                            break;
-                                        }
-                                    } // GPUDevice
                                 } // VGADevice
                             }
                         } // if !EFI_ERROR (XStatus)
                     } // if HandleType[i] & EFI_HANDLE_TYPE_DEVICE_HANDLE
 
-                    // Temp from Clover START
                     if (!FoundGOP) {
                         XStatus = refit_call5_wrapper (
                             gBS->LocateHandleBuffer,
