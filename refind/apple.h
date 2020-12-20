8443a15aaf2b422f658cf9b68a86bf4fc1b8f9e1
dakanji <dakanji@users.noreply.github.com>
2020-12-20 06:38:34 +0300

    Add Apple NVRAM Access Flags

--- refind/apple.h
+++ refind/apple.h
@@ -23,6 +23,10 @@
 
 // Apple's GUID
 #define APPLE_GUID { 0x7c436110, 0xab2a, 0x4bbb, { 0xa8, 0x80, 0xfe, 0x41, 0x99, 0x5c, 0x9f, 0x82 } };
+
+// Apple's NVRAM ACCESS FLAGS
+#define APPLE_FLAGS EFI_VARIABLE_BOOTSERVICE_ACCESS|EFI_VARIABLE_RUNTIME_ACCESS|EFI_VARIABLE_NON_VOLATILE;
+
 // These codes are returned in the first byte of the csr-active-config variable
 #define CSR_ALLOW_UNTRUSTED_KEXTS            0x01
 #define CSR_ALLOW_UNRESTRICTED_FS            0x02
