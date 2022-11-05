d5f2887567646676e963fb561edf2949265a5a42
dakanji <dakanji@users.noreply.github.com>
2022-11-05 16:27:22 +0300

    Housekeeping

--- filesystems/zstd/zstd_decompress.c
+++ filesystems/zstd/zstd_decompress.c
@@ -807,16 +807,21 @@ static seq_t ZSTD_decodeSequence(seqState_t *seqState)
 	U32 const ofBits = ofCode;
 	U32 const totalBits = llBits + mlBits + ofBits;
 
-	static const U32 LL_base[MaxLL + 1] = {0,  1,  2,  3,  4,  5,  6,  7,  8,    9,     10,    11,    12,    13,     14,     15,     16,     18,
-					       20, 22, 24, 28, 32, 40, 48, 64, 0x80, 0x100, 0x200, 0x400, 0x800, 0x1000, 0x2000, 0x4000, 0x8000, 0x10000};
+	static const U32 LL_base[MaxLL + 1] = {
+		0,  1,  2,  3,  4,  5,  6,  7,  8,    9,     10,    11,    12,    13,     14,     15,     16,     18,
+		20, 22, 24, 28, 32, 40, 48, 64, 0x80, 0x100, 0x200, 0x400, 0x800, 0x1000, 0x2000, 0x4000, 0x8000, 0x10000};
 
-	static const U32 ML_base[MaxML + 1] = {3,  4,  5,  6,  7,  8,  9,  10,   11,    12,    13,    14,    15,     16,     17,     18,     19,     20,
-					       21, 22, 23, 24, 25, 26, 27, 28,   29,    30,    31,    32,    33,     34,     35,     37,     39,     41,
-					       43, 47, 51, 59, 67, 83, 99, 0x83, 0x103, 0x203, 0x403, 0x803, 0x1003, 0x2003, 0x4003, 0x8003, 0x10003};
+	static const U32 ML_base[MaxML + 1] = {3,      4,      5,
+		6,     7,     8,     9,    10,    11,     12,     13,    14,      15,
+		16,   17,    18,    19,    20,    21,     22,     23,     24,     25,
+		26,   27,    28,    29,    30,    31,     32,     33,     34,     35,
+		37,   39,    41,    43,    47,    51,     59,     67,     83,     99,
+		0x83, 0x103, 0x203, 0x403, 0x803, 0x1003, 0x2003, 0x4003, 0x8003, 0x10003};
 
-	static const U32 OF_base[MaxOff + 1] = {0,       1,	1,	5,	0xD,      0x1D,      0x3D,      0x7D,      0xFD,     0x1FD,
-						0x3FD,   0x7FD,    0xFFD,    0x1FFD,   0x3FFD,   0x7FFD,    0xFFFD,    0x1FFFD,   0x3FFFD,  0x7FFFD,
-						0xFFFFD, 0x1FFFFD, 0x3FFFFD, 0x7FFFFD, 0xFFFFFD, 0x1FFFFFD, 0x3FFFFFD, 0x7FFFFFD, 0xFFFFFFD};
+	static const U32 OF_base[MaxOff + 1] = {0, 1,
+		1,       5,        0xD,      0x1D,     0x3D,     0x7D,     0xFD,       0x1FD,     0x3FD,
+		0x7FD,   0xFFD,    0x1FFD,   0x3FFD,   0x7FFD,   0xFFFD,   0x1FFFD,    0x3FFFD,   0x7FFFD,
+		0xFFFFD, 0x1FFFFD, 0x3FFFFD, 0x7FFFFD, 0xFFFFFD, 0x1FFFFFD, 0x3FFFFFD, 0x7FFFFFD, 0xFFFFFFD};
 
 	/* sequence */
 	{
@@ -1026,13 +1031,12 @@ static size_t ZSTD_decompressSequences(ZSTD_DCtx *dctx, void *dst, size_t maxDst
 	return op - ostart;
 }
 
-FORCE_INLINE seq_t ZSTD_decodeSequenceLong_generic(seqState_t *seqState, int const longOffsets)
-{
+FORCE_INLINE seq_t ZSTD_decodeSequenceLong_generic(seqState_t *seqState, int const longOffsets) {
 	seq_t seq;
 
 	U32 const llCode = FSE_peekSymbol(&seqState->stateLL);
 	U32 const mlCode = FSE_peekSymbol(&seqState->stateML);
-	U32 const ofCode = FSE_peekSymbol(&seqState->stateOffb); /* <= maxOff, by table construction */
+	U32 const ofCode = FSE_peekSymbol(&seqState->stateOffb); // <= maxOff, by table construction
 
 	U32 const llBits = LL_bits[llCode];
 	U32 const mlBits = ML_bits[mlCode];
@@ -1045,14 +1049,14 @@ FORCE_INLINE seq_t ZSTD_decodeSequenceLong_generic(seqState_t *seqState, int con
 	};
 
 	static const U32 ML_base[MaxML + 1] = {
-		3,  4,  5,  6,  7,  8,  9,  10,   11,    12,    13,    14,    15,     16,     17,     18,     19,     20,
-		21, 22, 23, 24, 25, 26, 27, 28,   29,    30,    31,    32,    33,     34,     35,     37,     39,     41,
+		3,  4,  5,  6,  7,  8,  9,  10,   11,    12,    13,    14,    15, 16, 17,     18,     19,     20,
+		21, 22, 23, 24, 25, 26, 27, 28,   29,    30,    31,    32,    33, 34, 35,     37,     39,     41,
 		43, 47, 51, 59, 67, 83, 99, 0x83, 0x103, 0x203, 0x403, 0x803, 0x1003, 0x2003, 0x4003, 0x8003, 0x10003
 	};
 
 	static const U32 OF_base[MaxOff + 1] = {
-		0,       1,	1,	5, 0xD,      0x1D,     0x3D,     0x7D,      0xFD,      0x1FD,     0x3FD,
-		0x7FD,   0xFFD,    0x1FFD,   0x3FFD,   0x7FFD,    0xFFFD,   0x1FFFD,   0x3FFFD,   0x7FFFD,
+		0, 1,    1, 5,     0xD,      0x1D,     0x3D,     0x7D,      0xFD,      0x1FD,     0x3FD,
+		0x7FD,   0xFFD,    0x1FFD,   0x3FFD,   0x7FFD,   0xFFFD,    0x1FFFD,   0x3FFFD,   0x7FFFD,
 		0xFFFFD, 0x1FFFFD, 0x3FFFFD, 0x7FFFFD, 0xFFFFFD, 0x1FFFFFD, 0x3FFFFFD, 0x7FFFFFD, 0xFFFFFFD
 	};
 
